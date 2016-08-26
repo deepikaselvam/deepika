5d2a77dd61a332fffc067fc770d8fa1e64951cdd
deepikaselvam <deepika.seee@gmail.com>
2016-08-26 18:28:24 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,20 @@
+# deepika
+
+public class Positive {
+
+	public static void main(String[] args) {
+	
+Scanner cs=new Scanner(System.in);
+int s=cs.nextInt();
+if(s>0)
+{
+	System.out.println("the no is positive");
+}
+else if(s<0)
+{
+	System.out.println("the no is negative");
+}
+
+	}
+
+}
